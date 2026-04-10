5330328de0fcd0bac53215e6a5d2db2c933eda8d
luanaferreira06 <luana.veiga.ferreira@escola.pr.gov.br>
2026-04-10 07:48:38 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,9 @@
+let nome        =    "luana";
+let idade       =      16;
+let altura      =     1.60;
+let trabalho    =     true;
+
+console.log(nome);
+console.log(altura);
+console.log(idade);
+console.log(trabalho);
